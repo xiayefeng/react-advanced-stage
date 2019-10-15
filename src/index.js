7c145c091a734fdb7db612935cb54ad80e358208
xiayefeng <xiayu_12@yeah.net>
2019-10-15 16:41:22 +0800

    add demo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./todo-list";
+import App from "./demo";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
